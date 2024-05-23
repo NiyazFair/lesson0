7848bf438ce9896822caccdb2745d28e174cfd32
NiyazFair <niyaz048@gmail.com>
2024-05-23 11:05:10 +0300

    Add files via upload

---
+++ задание 2.py
@@ -0,0 +1,8 @@
+first = 20
+second = 7
+print(first + second)
+print(first - second)
+summa = 27
+diff = 13
+print(summa)
+print(diff)
---
+++ задание 3.py
@@ -0,0 +1,5 @@
+first = 2
+second = 4
+third = 6
+mean = ((2 + 4 + 6)/3)
+print(mean)
